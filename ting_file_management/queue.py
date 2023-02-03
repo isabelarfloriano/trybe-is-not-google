b514a561f2d0ec80f43751e67c88fa32fbe0f91a
Isabela Rozani Floriano <irf161295@gmail.com>
2023-02-03 14:53:18 -0300

    feat: start the project

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,7 +3,7 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        """Inicialize sua estrutura aqui."""
 
     def __len__(self):
         """Aqui irá sua implementação"""
