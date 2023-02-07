1f3602e940296f4b3a96bd38e7f24f1ca0982643
Isabela Rozani Floriano <irf161295@gmail.com>
2023-02-07 19:31:24 -0300

    feat: add function to show a preview at file_process module

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -33,4 +33,7 @@ def remove(instance):
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        return print(instance.search(position), file=sys.stdout)
+    except IndexError:
+        return print("Posição inválida", file=sys.stderr)
