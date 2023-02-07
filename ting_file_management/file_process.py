a722303f7886d152434f8abfa6c50aa1418780b4
Isabela Rozani Floriano <irf161295@gmail.com>
2023-02-07 12:04:36 -0300

    fix: remove whitescape

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -8,17 +8,18 @@ def process(path_file, instance):
     original_file = txt_importer(path_file)
     dict_file = {
         "nome_do_arquivo": path_file,
-        "qtd_linhas": len(original_file),      
+        "qtd_linhas": len(original_file),
         "linhas_do_arquivo": original_file,
     }
 
     for i in range(0, len(instance)):
         if instance.search(i)["nome_do_arquivo"] == path_file:
             return None
-            
+
     instance.enqueue(dict_file)
     print(dict_file, file=sys.stdout)
     return dict_file
+
 
 def remove(instance):
     """Aqui irá sua implementação"""
