fb46224287806837d9d92bde2baa86d9063f3366
Isabela Rozani Floriano <irf161295@gmail.com>
2023-02-06 21:21:36 -0300

    feat: implements reading module using '\n' as splitter

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,11 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        print("Formato inválido", file=sys.stderr)
+    try:
+        with open(path_file) as file:
+            return file.read().split("\n")
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
