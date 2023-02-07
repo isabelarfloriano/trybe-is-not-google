d2305c492b9815cb3de59cef8562faf484558843
Isabela Rozani Floriano <irf161295@gmail.com>
2023-02-07 12:00:48 -0300

    feat: implements function that transforms content and add to queue

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,6 +1,24 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.file_management import txt_importer
 
+
+import sys
+
+
+def process(path_file, instance):
+    original_file = txt_importer(path_file)
+    dict_file = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(original_file),      
+        "linhas_do_arquivo": original_file,
+    }
+
+    for i in range(0, len(instance)):
+        if instance.search(i)["nome_do_arquivo"] == path_file:
+            return None
+            
+    instance.enqueue(dict_file)
+    print(dict_file, file=sys.stdout)
+    return dict_file
 
 def remove(instance):
     """Aqui irá sua implementação"""
