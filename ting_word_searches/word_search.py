bb870991d9c136f0050efc2cd81986b48e83d6e5
Isabela Rozani Floriano <irf161295@gmail.com>
2023-02-09 19:45:08 -0300

    feat: create function that check for the existence of a word in all processed files

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,19 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    for i in range(0, len(instance)):
+        file = instance.search(i)
+        lines = {
+            "palavra": word,
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": [],
+        }
+        for line in file:
+            if word.casefold() in line.casefold():
+                lines["ocorrencias"].append({"linha": file.index(line) + 1})
+        if lines["ocorrencias"]:
+            result.append(lines)
+
+    return result
 
 
 def search_by_word(word, instance):
