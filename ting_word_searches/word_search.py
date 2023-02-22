f016536f6488545e14b2bad49dd17d7946f98cab
Isabela Rozani Floriano <irf161295@gmail.com>
2023-02-22 18:12:47 -0300

    feat: implements search by word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -17,4 +17,13 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = exists_word(word, instance)
+
+    for i, result in enumerate(result):
+        for ocorrencias in result["ocorrencias"]:
+            lines = instance.result(i)
+            ocorrencias["conteudo"] = (
+                lines["linhas_do_arquivo"][ocorrencias["linha"] - 1]
+            )
+
+    return result
