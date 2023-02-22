09ea71ea434c4f52f02f42c83b23d68401b6dd26
Isabela Rozani Floriano <irf161295@gmail.com>
2023-02-22 19:39:22 -0300

    fix: lint

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,6 +1,7 @@
 from ting_file_management.priority_queue import PriorityQueue
 import pytest
 
+
 def test_basic_priority_queueing():
     queue = PriorityQueue()
     queue.enqueue({"qtd_linhas": 1})
