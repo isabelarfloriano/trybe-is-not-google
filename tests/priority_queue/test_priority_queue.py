735fe3d30f4f92a5f39828540b0b82547cd96f95
Isabela Rozani Floriano <irf161295@gmail.com>
2023-02-22 19:15:10 -0300

    test: create test basic priority queueing

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,24 @@
-# from ting_file_management.priority_queue import PriorityQueue
-
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    queue = PriorityQueue()
+    queue.enqueue({"qtd_linhas": 1})
+    queue.enqueue({"qtd_linhas": 12})
+    queue.enqueue({"qtd_linhas": 9})
+
+    assert len(queue) == 3
+    assert len(queue.regular_priority) == 2
+    assert len(queue.high_priority) == 1
+
+    queue.dequeue() == {"qtd_linhas": 12}
+
+    assert len(queue) == 2
+    assert len(queue.regular_priority) == 2
+    assert len(queue.high_priority) == 0
+
+    assert queue.search(0) == {"qtd_linhas": 12}
+    assert queue.search(1) == {"qtd_linhas": 9}
+
+    with pytest.raises(IndexError):
+        queue.search(99)
